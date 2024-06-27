ac2a06af371cd41a4a268c9125b27230277639ee
Ayush Gupta <ayush2425.rk@gmail.com>
2024-06-28 00:30:40 +0530

    Debug5

---
+++ CodeHelp/Debug/Debug5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include<cmath>
+#include<limits.h>
+#include <vector>
+#include<string>
+using namespace std;
+
+int mystoi(string s) {
+        int len = s.size();
+        double num = 0;
+        int i=0;
+        while(s[i] == ' '){
+            i++;
+        }
+        bool negative = s[i] == '-';
+        negative == true ? i++ : i;
+        while(i < len && s[i] >= '0' && s[i] <= '9'){
+            num = num*10 + s[i];
+            i++;
+        }
+        num = negative ? -num : num;
+        num = (num > INT_MAX) ? INT_MAX : num;
+        num = (num < INT_MIN) ? INT_MIN : num;
+        return num;
+    }
