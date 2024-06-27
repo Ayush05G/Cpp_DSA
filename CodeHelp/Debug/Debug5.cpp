8d83f76d9da4867fa82c7fe46f82827e6d62903d
Ayush Gupta <ayush2425.rk@gmail.com>
2024-06-28 02:16:45 +0530

    Question 4

--- CodeHelp/Debug/Debug5.cpp
+++ CodeHelp/Debug/Debug5.cpp
@@ -53,6 +53,27 @@ string addBinary(string a, string b) {
     return ans;
   }
 
+  void reverseString(char input[],int n,int start){
+    for(int i=start,j=n-1+start; i<j; i++,j--){
+        int temp = input[i];
+        input[i] = input[j];
+        input[j] = temp;
+    }
+}
+
+void reverseEachWord(char input[]) {
+    int start=0,count=1,i=0;
+    while(input[i]!=0){
+        i++;
+        count++;
+        if(input[i]==' '||input[i]=='\0'){
+            reverseString(input,count-1,start);
+            start = i+1;
+            count = 0;
+        }
+    }
+}
+
     int main(){
         // string s;
         // getline(cin, s);
@@ -64,13 +85,20 @@ string addBinary(string a, string b) {
         // string roman=intToRoman(num);
         // cout<<roman;
         
-        string a;
-        string b;
-        cout<<"Enter Number 1: ";
-        cin>>a;
-        cout<<"Enter Number 2: ";
-        cin>>b;
-        string add=addBinary(a,b);
-        cout<<add;
+        // string a;
+        // string b;
+        // cout<<"Enter Number 1: ";
+        // cin>>a;
+        // cout<<"Enter Number 2: ";
+        // cin>>b;
+        // string add=addBinary(a,b);
+        // cout<<add;
+
+        //Difficult to Understand
+        char input[100];
+        cin>>input;
+        reverseEachWord(input);
+        cout<<input;
+        
         return 0;
     }
