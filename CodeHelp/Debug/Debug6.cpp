dd4ccab8fb1366d4b2746bcdafe8f27b86269321
Ayush Gupta <ayush2425.rk@gmail.com>
2024-07-08 22:20:19 +0530

    Question 3

--- CodeHelp/Debug/Debug6.cpp
+++ CodeHelp/Debug/Debug6.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	int* ptr=0;
-	int a = 5;
-	ptr = &a;
-	cout << *ptr << endl;
-	return 0;
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+ 
+    for (int i = 2; i <= n / 2; i++)
+       { if (n % i==0)
+            return false;
+ }
+    return true;
 }
