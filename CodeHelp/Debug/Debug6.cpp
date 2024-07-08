89f8bdae0d253626b56281e06f697a69357a6902
Ayush Gupta <ayush2425.rk@gmail.com>
2024-07-09 00:34:21 +0530

    Question 2(Done)

--- CodeHelp/Debug/Debug6.cpp
+++ CodeHelp/Debug/Debug6.cpp
@@ -1,18 +1,61 @@
 #include <iostream>
+#include <algorithm>
+#include<cmath>
+#include<limits.h>
 #include <vector>
+#include<string>
 using namespace std;
 
-void SieveOfEratosthenes(int n) 
+void fillPrimes(vector<int>& prime, int high)
 {
-    vector<bool> prime(n+1, true);
-  
-    for (int p = 2; p * p <= n; p++) {
-            for (int i = p * p; i <= n; i += p) 
-                prime[i] = false; 
-        } 
+    vector<bool> ck(high+1, true);
+    for (int i = 2; (i * i) <= high; i++) {
+        if (ck[i] == true) {
+            for (int j = i * i; j <= sqrt(high); j = j + i) {
+                ck[j] = false;
+            }
+        }
+    }
+    for (int i = 2; i * i <= high; i++) {
+        if (ck[i] == true) {
+            prime.push_back(i);
+        }
+    }
+}
 
-  
-    for (int p = 2; p <= n; p++) 
-        if (prime[p]) 
-            cout << p << " "; 
+void segmentedSieve(int low, int high)
+{
+    if (low<2 and high>=2){
+        low = 2;
+    }
+    vector<bool> prime(high-low+1, true);
+    vector<int> chprime;
+    fillPrimes(chprime, high);
+    for (int i : chprime) {
+        int lower = (low / i);
+        if (lower <= 1) {
+            lower = i + i;
+        }
+        else if (low % i) {
+            lower = (lower * i) + i;
+        }
+        else {
+            lower = (lower * i);
+        }
+        for (int j = lower; j <= high; j = j + i) {
+            prime[j-low] = false;
+        }
+    }
+   
+    for (int i = low; i <= high; i++) {
+        if (prime[i-low] == true) {
+            cout << (i) << " ";
+        }
+    }
+}
+
+int main(){
+    int low = 10, high = 50;
+    segmentedSieve(low, high);
+    return 0;
 }
