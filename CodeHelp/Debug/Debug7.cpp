3d2b023862ac6d7096f793d52cda9a1ecfb93cb9
Ayush Gupta <ayush2425.rk@gmail.com>
2024-07-13 03:03:29 +0530

    Question 2

--- CodeHelp/Debug/Debug7.cpp
+++ CodeHelp/Debug/Debug7.cpp
@@ -12,9 +12,26 @@ bool isPowerOfFour(int n) {
         return isPowerOfFour(n/4);
     }
 
+    double myPow(double x, int n) {
+
+       int p = abs(n);
+       double ans = 1.0;
+       while(p>=1){
+        p-- ;
+          ans *= x;
+       }
+       return n<0 ? 1/ans : ans;    
+    }
+
 int main(){
+    // int n;
+    // cin>>n;
+    // cout<<isPowerOfFour(n);
+
+    double x;
     int n;
-    cin>>n;
-    cout<<isPowerOfFour(n);
+    cin>>x>>n;
+    cout<<myPow(x,n);
+
     return 0;
 }
