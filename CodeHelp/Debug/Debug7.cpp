72bd55d43fe214f184b5f2b814152731f39adcb4
Ayush Gupta <ayush2425.rk@gmail.com>
2024-07-13 04:02:12 +0530

    Question 3(Done)

--- CodeHelp/Debug/Debug7.cpp
+++ CodeHelp/Debug/Debug7.cpp
@@ -23,6 +23,49 @@ bool isPowerOfFour(int n) {
        return n<0 ? 1/ans : ans;    
     }
 
+    int subset(int input[], int n, int output[][50], int k) {
+    if(n==0){
+        output[0][0] = 0;
+        return 1;
+    }
+    
+    int small = subset(input+1,n-1,output,k);
+    int count = 0;
+    
+    for(int i=0;i<small;i++){
+        output[small+count][1] = input[0];
+        int sum = input[0];
+        int l = 0;
+        for(int j=2;output[small-count-1][j-1]!=0;j++){
+            l++;
+            output[small+count][j] = output[small-count-1][j-1];
+            sum = sum + output[small-count-1][j-1];
+        }
+        output[small+count][0] = -1;
+        if(sum==k){
+            output[small+count][0] = l+1;
+        }
+        count++;
+    }
+    return small+count;
+}
+
+int subsetSumToK(int input[], int n, int output[][50], int k) {
+    int temp[1110000][50];
+    int size = subset(input,n,temp,k);
+    int m=0;
+    for(int i=0;i<size;i++){
+        if(temp[i][0]>0){
+            for(int j=0;j<=temp[i][0];j++){
+                output[m][j] = temp[i][j];
+            }
+            m++;
+        }
+    }
+    
+    return m;
+}
+
     int returnPermutations(string input, string output[]){
     int count = 0;
     string created;
@@ -89,13 +132,26 @@ int main(){
     // cin>>x>>n;
     // cout<<myPow(x,n);
 
-    string input;
-    cin>>input;
-    string output[1000];
-    int count = returnPermutations(input,output);
-    for(int i=0;i<count;i++){
-        cout<<output[i]<<endl;
+    int input[20],length, output[1000][50], k;
+    cin >> length;
+    for(int i=0; i < length; i++)
+        cin >> input[i];
+    cin >> k;
+    int size = subsetSumToK(input, length, output, k);
+    for( int i = 0; i < size; i++) {
+        for( int j = 1; j <= output[i][0]; j++) {
+            cout << output[i][j] << " ";
+        }
+        cout << endl;
     }
+
+    // string input;
+    // cin>>input;
+    // string output[1000];
+    // int count = returnPermutations(input,output);
+    // for(int i=0;i<count;i++){
+    //     cout<<output[i]<<endl;
+    // }
 
     // char input[100];
     // cin.getline(input, 100);
