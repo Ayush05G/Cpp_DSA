1a7d0f6606a3ffef4abee32225acd28c68ca7b79
Ayush Gupta <ayush2425.rk@gmail.com>
2024-07-13 03:14:31 +0530

    Question 5

--- CodeHelp/Debug/Debug7.cpp
+++ CodeHelp/Debug/Debug7.cpp
@@ -23,15 +23,36 @@ bool isPowerOfFour(int n) {
        return n<0 ? 1/ans : ans;    
     }
 
+    void removeConsecutiveDuplicates(char *input) {
+	if(input[0] == '\0'){
+        return;
+    }
+	if(input[0] == input[1]){
+        int i = 2;
+        for(;input[i]!='\0';i++){
+            input[i-1] = input[i]; 
+        }
+        input[i-1] = input[i];
+        removeConsecutiveDuplicates(input);
+    }
+    else
+    removeConsecutiveDuplicates(input+1);
+}
+
 int main(){
     // int n;
     // cin>>n;
     // cout<<isPowerOfFour(n);
 
-    double x;
-    int n;
-    cin>>x>>n;
-    cout<<myPow(x,n);
+    // double x;
+    // int n;
+    // cin>>x>>n;
+    // cout<<myPow(x,n);
+
+    char input[100];
+    cin.getline(input, 100);
+    removeConsecutiveDuplicates(input);
+    cout << input << endl;
 
     return 0;
 }
