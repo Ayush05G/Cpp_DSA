57a9a484814fc6fb8cfbd459c6c614c7493a1019
Ayush Gupta <ayush2425.rk@gmail.com>
2024-07-15 00:21:27 +0530

    New method Add

--- CodeHelp/Recursion/Assignment/RemoveSubstring.cpp
+++ CodeHelp/Recursion/Assignment/RemoveSubstring.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Iterative Solution
 class Solution {
 public:
     string removeOccurrences(string full, string pattern) {
@@ -10,5 +11,26 @@ public:
         }
         return full;
 
+    }
+};
+
+// Recursive Solution
+class Solution {
+public:
+    void RemoveOccur(string&s,string&part){
+        int found=s.find(part);
+        if(found!=string::npos){
+            string left=s.substr(0,found);
+            string right=s.substr(found+part.length(),s.length());
+            s=left+right;
+            RemoveOccur(s,part);
+        }
+        else 
+        return;
+    }
+
+    string removeOccurrences(string s, string part) {
+        RemoveOccur(s,part);
+        return s;
     }
 };
