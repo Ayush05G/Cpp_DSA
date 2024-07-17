dbf2292240e5de70e0bcd1e7eccdec98fea71f0e
Ayush Gupta <ayush2425.rk@gmail.com>
2024-07-18 02:46:31 +0530

    LC27

---
+++ Q/LC27.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  int removeElement(vector<int>& nums, int val) {
+    int i = 0;
+
+    for (const int num : nums)
+      if (num != val)
+        nums[i++] = num;
+
+    return i;
+  }
+};
