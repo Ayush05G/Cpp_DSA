1d3fe602b2c2ebb6478f2ca9c770a921fa48fedb
Ayush Gupta <ayush2425.rk@gmail.com>
2024-07-21 03:06:44 +0530

    Question 3

--- CodeHelp/Debug/Debug8.cpp
+++ CodeHelp/Debug/Debug8.cpp
@@ -11,6 +11,29 @@ void merge_sort(int arr[], int start, int end) {
     //merge(arr, start, mid, end);
 }
 
+int ceilSearch(int arr[], int low, int high, int x) {
+	int mid;
+	if (x < arr[low])
+		return low;
+	if (x >= arr[high])
+		return -1;
+	mid = (low + high) / 2;
+	if (arr[mid] == x)
+		return mid;
+	else if (arr[mid] < x) {
+		if (mid + 1 <= high && x <= arr[mid + 1])
+			return mid + 1;
+		else
+			return ceilSearch(arr, mid + 1, high, x);
+	}
+	else {
+		if (mid - 1 >= low && x > arr[mid - 1])
+			return mid;
+		else
+			return ceilSearch(arr, low, mid - 1, x);
+	}
+}
+
 int power(int x, int n){
     if(n == 0) return 1;
     if(x == 0) return 0;
@@ -23,8 +46,18 @@ int power(int x, int n){
 }
 
 int main() {
-    int result=power(2, 3);
-    cout << result << endl;
-    
+    int arr[] = {1, 2, 8, 10, 10, 12, 19};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int x = 5;
+    int index = ceilSearch(arr, 0, n-1, x);
+    if(index == -1){
+        cout << "Ceil of " << x << " doesn't exist in the array" << endl;
+    } else{
+        cout << "Ceil of " << x << " is " <<index<< endl;
+    }
+
+    // int result=power(2, 3);
+    // cout << result << endl;
+
     return 0;
 }
