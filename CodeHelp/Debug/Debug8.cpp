24e74c4a59e1f14d0dba72267278aa271d9b6947
Ayush Gupta <ayush2425.rk@gmail.com>
2024-07-21 02:57:31 +0530

    Question 6

--- CodeHelp/Debug/Debug8.cpp
+++ CodeHelp/Debug/Debug8.cpp
@@ -8,9 +8,23 @@ void merge_sort(int arr[], int start, int end) {
     int mid = (start + end) / 2;
     merge_sort(arr, start, mid-1);
     merge_sort(arr, mid+1, end);
-    merge(arr, start, mid, end);
+    //merge(arr, start, mid, end);
+}
+
+int power(int x, int n){
+    if(n == 0) return 1;
+    if(x == 0) return 0;
+    int temp = power(x, n/2);
+    if(n % 2==0){
+        return temp*temp;
+    } else{
+        return temp*temp*x;
+    }
 }
 
 int main() {
+    int result=power(2, 3);
+    cout << result << endl;
+    
     return 0;
 }
