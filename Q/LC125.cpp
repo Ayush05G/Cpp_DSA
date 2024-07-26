5ec6f979ce4a4fd594a5451f03e4a74b55b07ab5
Ayush Gupta <ayush2425.rk@gmail.com>
2024-07-26 18:13:29 +0530

    LC125

---
+++ Q/LC125.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  bool isPalindrome(string s) {
+    int l = 0;
+    int r = s.length() - 1;
+
+    while (l < r) {
+      while (l < r && !isalnum(s[l]))
+        ++l;
+      while (l < r && !isalnum(s[r]))
+        --r;
+      if (tolower(s[l]) != tolower(s[r]))
+        return false;
+      ++l;
+      --r;
+    }
+
+    return true;
+  }
+};
