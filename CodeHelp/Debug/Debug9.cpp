ae6f98d40e9b1d0a885505eda2a49c56d5af0e80
Ayush Gupta <ayush2425.rk@gmail.com>
2024-07-27 22:50:10 +0530

    Question 6(Done)

--- CodeHelp/Debug/Debug9.cpp
+++ CodeHelp/Debug/Debug9.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
-using namespace std;
-
 class Animal {
+protected:
+    std::string name;
+
 public:
-    virtual void speak() { cout << "Animal speaks" << endl; }
+    Animal(const std::string& n) : name(n) {}
+    virtual void makeSound(){
+        std::cout << "Animal named " << name << " says something!" << std::endl;
+    };
+};
+
+class Cat : public Animal {
+public:
+    Cat(const std::string& n) : Animal(n) {}
+    void makeSound() {
+        std::cout << "Cat named " << name << " says Meow!" << std::endl;
+    }
 };
 
 class Dog : public Animal {
 public:
-    void speak() { cout << "Dog barks" << endl; }
+    Dog(const std::string& n) : Animal(n) {}
+    void makeSound() {
+        std::cout << "Dog named " << name << " says Woof!" << std::endl;
+    }
 };
 
 int main() {
-    Animal* a = new Dog;
-    a->speak();
-    delete a;
+    Cat cat("Whiskers");
+    Dog dog("Rover");
+    cat.makeSound();
+    dog.makeSound();
     return 0;
 }
