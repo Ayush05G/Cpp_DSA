be82468c585bdfed839ce276a1ef8310cf63a047
Ayush Gupta <ayush2425.rk@gmail.com>
2024-08-01 02:48:30 +0530

    LC217

---
+++ Q/LC217.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        return numSet.size() < nums.size();
+    }
+};
