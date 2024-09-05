be5f65d890ab8a95b74f32c618e5d6bf4ce70180
Ayush Gupta <ayush2425.rk@gmail.com>
2024-09-06 00:51:33 +0530

    feat: Add algorithm header and implementation files
    This commit adds the header file "algorithm.h" and the implementation file "algorithm.cpp" to the project. The "algorithm.h" file contains the declaration of the "sort" function, while the "algorithm.cpp" file defines the implementation of the "sort" function. These files are necessary for the project to compile and run successfully.

---
+++ CodeHelp/TalkAboutHeaderFiles/algorithm.cpp
@@ -0,0 +1,9 @@
+#include "algorithm.h"
+#include <iostream>
+
+void sort(int arr[], int n)
+{
+    // palak ka international patented sort algo.
+    std::cout << "palak ka international patented sort algo." << std::endl;
+    return;
+}
---
+++ CodeHelp/TalkAboutHeaderFiles/algorithm.h
@@ -0,0 +1,7 @@
+// algorithm.h
+#ifndef ALGORITHM_H // guard
+#define ALGORITHM_H
+
+void sort(int arr[], int n); // declaration
+
+#endif // ALGORITHM_H
