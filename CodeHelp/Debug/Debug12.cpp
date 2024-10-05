56054b2363c420ad60d5ccfee7a2d5a38c19a277
Ayush Gupta <ayush2425.rk@gmail.com>
2024-10-06 02:21:03 +0530

    Question 5(Done)

--- CodeHelp/Debug/Debug12.cpp
+++ CodeHelp/Debug/Debug12.cpp
@@ -4,37 +4,137 @@
 using namespace std;
 
 
-int maxSubarraySumCircular(vector<int>& nums) {
-        const int n = nums.size();
-        vector<int> rightMax(n);
-        rightMax[n - 1] = nums[n - 1];
-        int suffixSum = nums[n - 1];
-
-        for (int i = n - 2; i >= 0; --i) {
-            suffixSum += nums[i];
-            rightMax[i] = max(rightMax[i + 1], suffixSum);
-        }
-
-        int maxSum = nums[0];
-        int specialSum = nums[0];
-        int curMax = 0;
-        int prefixSum = 0;
-        for (int i = 0; i < n; ++i) {
-            // This is Kadane's algorithm.
-            curMax = max(curMax, 0) + nums[i];
-            maxSum = max(maxSum, curMax);
-
-            prefixSum += nums[i];
-            if (i + 1 < n) {
-                specialSum = max(specialSum, prefixSum + rightMax[i + 1]);
-            }
-        }
-
-        return max(maxSum, specialSum);
+class MyCircularDeque {
+    int *arr;
+    int size;
+    int front;
+    int rear;
+public:
+    MyCircularDeque(int k) {
+        size=k;
+        arr=new int [size];
+        front =-1;
+        rear =-1;
     }
-     
-int main() {
-    vector<int> nums = {1, -2, 3, -2};
-    cout << maxSubarraySumCircular(nums) << endl;
+    
+    bool insertFront(int value) {
+         if(isFull()){
+            // cout<<"queue is full";
+            return false;
+        }
+        // for first element push
+         if(front==-1 && rear==-1){
+            front = rear =0;
+        }
+        else if(front == 0){  
+           front=size-1;
+        }
+        else{
+            front--;
+        }
+        arr[front]=value;
+
+        return true;
+    }
+    
+    bool insertLast(int value) {
+          if(isFull()){
+            // cout<<"queue is full";
+            return false;
+        }
+        // for first element push
+         if(front==-1 && rear==-1){
+            front = rear =0;
+        }
+        else{  
+            rear=(rear+1)%size;
+        }
+        arr[rear]=value;
+
+        return true;
+    }
+    
+    bool deleteFront() {
+        if(isEmpty()){ //to check queue is empty
+            return false;
+        }
+        int ans=arr[front];
+        arr[front]=-1;
+         if (front==rear){ //single element is present
+            front=rear=-1;
+        }
+        else {
+            front=(front+1)%size;
+        }
+        return true;
+    }
+    
+    bool deleteLast() {
+         if(isEmpty()){
+            return false;
+        }
+        int ans=arr[rear];
+        arr[rear]=-1;
+
+        if(front==rear){
+            front=rear=-1;
+        }
+        else if(rear==0){
+            rear=size-1;
+        }
+        else{
+            rear--;
+        }
+        return true;
+    }
+    
+    int getFront() {
+         if(isEmpty()){
+            return -1;
+        }
+        else{
+            return arr[front];
+        }
+    }
+    
+    int getRear() {
+        if(isEmpty()){
+            return -1;
+        }
+        else{
+            return arr[rear];
+        }
+    }
+    
+    bool isEmpty() {
+        if(front==-1 && rear==-1){ //to check queue is empty
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    bool isFull() {
+        if((rear+1)%size==front){
+            // cout<<"queue is full";
+            return true;
+         } else {
+            return false;
+         }
+    }
+};
+
+int main(){
+    MyCircularDeque* obj = new MyCircularDeque(3);
+    cout<<obj->insertLast(1)<<endl;
+    cout<<obj->insertLast(2)<<endl;
+    cout<<obj->insertFront(3)<<endl;
+    cout<<obj->insertFront(4)<<endl;
+    cout<<obj->getRear()<<endl;
+    cout<<obj->isFull()<<endl;
+    cout<<obj->deleteLast()<<endl;
+    cout<<obj->insertFront(4)<<endl;
+    cout<<obj->getFront()<<endl;
     return 0;
 }
