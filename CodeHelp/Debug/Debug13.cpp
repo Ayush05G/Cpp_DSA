a74b0d63348705ad154e70fc62144144674656bf
Ayush Gupta <ayush2425.rk@gmail.com>
2024-11-07 17:42:07 +0530

    Question 5(Done)

--- CodeHelp/Debug/Debug13.cpp
+++ CodeHelp/Debug/Debug13.cpp
@@ -1,6 +1,13 @@
-int countNodes(TreeNode* root) {
-         if(root == NULL) return 0;
-        int left = countNodes(root->left);
-        int right = countNodes(root->right);
-        return left + right+1;
+int diameterOfBinaryTree(TreeNode* root) {
+        int d=0;
+        rec(root, d);
+        return d;
+    }
+    
+    int rec(TreeNode* root, int &d) {
+        if(root == NULL) return 1;
+        int ld = rec(root->left, d)+1;
+        int rd = rec(root->right, d)+1;
+        d=max(d,ld+rd);
+        return max(ld,rd)+1;
     }
