68ec08b392444cb443d600380980496ca879586a
Ayush Gupta <ayush2425.rk@gmail.com>
2024-11-09 15:05:19 +0530

    Question 5(Done)

--- CodeHelp/Debug/Debug14.cpp
+++ CodeHelp/Debug/Debug14.cpp
@@ -8,21 +8,44 @@ struct TreeNode {
     TreeNode *right;
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
 
-TreeNode* sortedArrayToBST(vector<int>& nums) {
-         if(nums.size()==0)return NULL;
-         if(nums.size()==1) return new TreeNode(nums[0]);
-         int middle = nums.size()/2;
-         TreeNode* root = new TreeNode(nums[middle]);
-         vector<int> leftsub(nums.begin(), nums.begin()+middle);
-         vector<int> rightsub(nums.begin()+ middle+1, nums.end());
-         root->left = sortedArrayToBST(leftsub);
-         root->right = sortedArrayToBST(rightsub);
-         return root;
-     }
+TreeNode * ans =new TreeNode(0);
+    TreeNode* solve(vector<int> &node,int s,int e,int n){
+        if(s>e){
+            return NULL;
+        }
+        int mid=(s+e+1)/2;
+        TreeNode* p=new TreeNode(node[mid]);
+       
+         p->left=solve(node,s,mid-1,n);
+         p->right=solve(node,mid+1,e,n);
+        return p;
+        
+    }
+    TreeNode* sortedListToBST(ListNode* head) {
+        vector<int> v;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            v.push_back(temp->val);
+            temp=temp->next;
+        }
+        TreeNode* tmp= new TreeNode(0);
+       
+       return solve(v,0,v.size()-1,v.size()-1);
+        
+    }
 
 int main(){
-    vector<int> nums = {-10,-3,0,5,9};
-    TreeNode* root = sortedArrayToBST(nums);
+    ListNode* head=new ListNode(-10);
+    head->next=new ListNode(-3);
+    head->next->next=new ListNode(0);
+    head->next->next->next=new ListNode(5);
+    head->next->next->next->next=new ListNode(9);
+    TreeNode* root=sortedListToBST(head);
     return 0;
 }
