2810da25fd57f9378f2e978ef1aff1b98c41dda2
Ayush Gupta <ayush2425.rk@gmail.com>
2024-11-09 14:45:48 +0530

    Question 2

--- CodeHelp/Debug/Debug14.cpp
+++ CodeHelp/Debug/Debug14.cpp
@@ -1,18 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int numTrees(int n) {
-        if(n==0 || n==1) return 1;
-        int ans = 0;
-        for(int i=1;i<=n;i++){
-            ans+=numTrees(i-1)*numTrees(n-i);
-        }
-        return ans;
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+void inorder(TreeNode* cur, TreeNode*& prev, TreeNode*& first, TreeNode*& sec) {
+    if (!cur) { return; }
+    inorder(cur->left, prev, first, sec);
+    if (prev != NULL && cur->val < prev->val) {
+        if (!first) { first = prev; }
+        sec = cur;
     }
+    prev = cur;
+    inorder(cur->right, prev, first, sec);
+}
+
+void recoverTree(TreeNode* root) {
+    TreeNode* prev = nullptr;
+    TreeNode* first = nullptr;
+    TreeNode* sec = nullptr;
+    inorder(root, prev, first, sec);
+    int temp = first->val;
+    first->val = sec->val;
+    sec->val = temp;
+}
 
 int main(){
-    int n;
-    cin>>n;
-    cout<<numTrees(n);
+    // Create a tree
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(3);
+    root->left->right = new TreeNode(2);
+    recoverTree(root);
     return 0;
 }
