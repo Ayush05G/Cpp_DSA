8c553275e586b83ab06800f847797985f55f00ce
Ayush Gupta <ayush2425.rk@gmail.com>
2024-11-11 03:58:34 +0530

    Question 3

--- CodeHelp/Debug/Debug15.cpp
+++ CodeHelp/Debug/Debug15.cpp
@@ -1,35 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*k closest points to the origin using a priority queue*/
-vector<pair<int, int>> kClosestPoints(vector<pair<int, int>> points, int n, int k)
-{
-    vector<pair<int, int>> ans;
-    priority_queue<int, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> p;
-
-    for (int i = 0; i < n; i++)
-    {
-        int distance = points[i].first * points[i].first + points[i].second * points[i].second;
-        p.push({distance, {points[i].first, points[i].second}});
+vector<int> topKFrequent(vector<int>& nums, int k) {
+    unordered_map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+    unordered_map<int,int>::iterator it=m.begin();
+    priority_queue<pair<int,int>>pq;
+        for(;it!=m.end();it++){
+            pq.push(make_pair(it->second, it->first));
+        }
+        vector<int>ans;
+        int a=0;
+        while(a<k){
+            ans.push_back(pq.top().second);
+            pq.pop();
+            a++;
+        }
+        return ans;
     }
-    while (k--)
-    {
-        auto x = p.top();
-        ans.push_back({x.second.first, x.second.second});
-                p.pop();
-    }
-    return ans;
-}
 
 int main()
 {
-    vector<pair<int, int>> points = {{1, 3}, {-2, 2}, {5, 8}, {0, 1}};
-    int n = points.size();
+    vector<int> nums = {1,1,1,2,2,3};
     int k = 2;
-    vector<pair<int, int>> ans = kClosestPoints(points, n, k);
-    for (auto x : ans)
-    {
-        cout << x.first << " " << x.second << endl;
+    vector<int> ans = topKFrequent(nums, k);
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
     }
     return 0;
 }
