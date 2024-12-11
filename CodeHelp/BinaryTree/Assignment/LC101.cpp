7948168d6c7e9184dc1b186601cc2c0ca29565d7
Ayush Gupta <ayush2425.rk@gmail.com>
2024-12-11 18:58:46 +0530

    Add solutions for binary tree problems: isSameTree and isSymmetric

---
+++ CodeHelp/BinaryTree/Assignment/LC101.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isMirror(TreeNode* p, TreeNode* q) {
+        if(!p && !q)
+        return true;
+        if (p && q){
+        return (p->val==q->val && isMirror(p->left ,q->right) && isMirror(p->right , q->left));
+        }
+        return false;
+        
+    }
+    bool isSymmetric(TreeNode* root) {
+        return isMirror(root->left, root->right);
+        
+    }
+};
