a79d7761b5309e56c7f43b4a8553b0a96dde60cd
Ayush Gupta <ayush2425.rk@gmail.com>
2024-12-21 05:29:40 +0530

    Debug 15

---
+++ CodeHelp/Debug/Debug16.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+/*
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+*/
+
+void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        unordered_set<int> setRows; 
+        unordered_set<int> setColumns; 
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j]==0){
+                    setRows.insert(i);
+                    setColumns.insert(j);
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(setRows.count(i) > 0 || setColumns.count(j) > 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+int main(){
+    vector<vector<int>> matrix = {{1,1,1},{1,0,1},{1,1,1}};
+    setZeroes(matrix);
+    for(int i=0; i<matrix.size(); i++){
+        for(int j=0; j<matrix[0].size(); j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
