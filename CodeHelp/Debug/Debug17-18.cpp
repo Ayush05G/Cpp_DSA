bc30bb2081ae4ae0e03b38998a2c01e8d02c7d57
Ayush Gupta <ayush2425.rk@gmail.com>
2024-12-27 23:50:00 +0530

    Question 4

--- CodeHelp/Debug/Debug17-18.cpp
+++ CodeHelp/Debug/Debug17-18.cpp
@@ -2,43 +2,44 @@
 using namespace std;
 
 /*
-Ugly numbers are numbers whose only prime factors are 2, 3 or 5. Given a number n, the task is to find nâ€™th Ugly number.
+Given 3 strings, the task is to find the longest common sub-sequence in all three given sequences.
 */
 
-unsigned getNthUglyNo(unsigned n)
-{
-    unsigned ugly[n]; 
-    unsigned i2 = 0, i3 = 0, i5 = 0;
-    unsigned next_multiple_of_2 = 2;
-    unsigned next_multiple_of_3 = 3;
-    unsigned next_multiple_of_5 = 5;
-    unsigned next_ugly_no = 1;
+int lcsOf3( string X, string Y, string Z, int m, int n, int o) {
+    int L[m+1][n+1][o+1];
+    for (int i=0; i<=m; i++)
+    {
+        for (int j=0; j<=n; j++)
+        {
+            for (int k=0; k<=o; k++)
+            {
+                if (i == 0 || j == 0||k==0)
+                    L[i][j][k] = 0;
  
-    ugly[0] = 1;
-    for (int i = 1; i < n; i++) {
-        next_ugly_no = min(
-            next_multiple_of_2,
-            min(next_multiple_of_3, next_multiple_of_5));
-        ugly[i] = next_ugly_no;
-        if (next_ugly_no == next_multiple_of_2) {
-            i2 = i2 + 1;
-            next_multiple_of_2 = ugly[i2] * 2;
+                else if (X[i-1] == Y[j-1] && X[i-1]==Z[k-1])
+                    L[i][j][k] = L[i-1][j-1][k-1] + 1;
+ 
+                else
+                    L[i][j][k] = max(max(L[i-1][j][k],
+                                         L[i][j-1][k]),
+                                     L[i][j][k-1]);
+            }
         }
-        if (next_ugly_no == next_multiple_of_3) {
-            i3 = i3 + 1;
-            next_multiple_of_3 = ugly[i3] * 3;
-        }
-        if (next_ugly_no == next_multiple_of_5) {
-            i5 = i5 + 1;
-            next_multiple_of_5 = ugly[i5] * 5;
-        }
-    }  
-    return next_ugly_no;
+    }
+    return L[m][n][o];
 }
 
 int main() 
 { 
-    int n = 150; 
-    cout << getNthUglyNo(n); 
+    string X = "AGGT12"; 
+    string Y = "12TXAYB"; 
+    string Z = "12XBA"; 
+  
+    int m = X.length(); 
+    int n = Y.length(); 
+    int o = Z.length(); 
+  
+    cout << "Length of LCS is " << lcsOf3(X, Y, Z, m, n, o); 
+  
     return 0; 
 }
