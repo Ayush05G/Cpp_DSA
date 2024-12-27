99e5c78b924a57ea1c67b0a1fcac1131ea4277ba
Ayush Gupta <ayush2425.rk@gmail.com>
2024-12-27 23:48:27 +0530

    Question 2

--- CodeHelp/Debug/Debug17-18.cpp
+++ CodeHelp/Debug/Debug17-18.cpp
@@ -2,58 +2,30 @@
 using namespace std;
 
 /*
-Given an array, the task is to find sum of maximum sum alternating subsequence starting with first element. Here alternating sequence means first decreasing, then increasing, then decreasing, â€¦ For example 10, 5, 14, 3 is an alternating sequence. 
+Given an array of integers of size N, you have to divide it into the minimum number of ā€strictly increasing subsequences.
+Take the following example where the sequence is {1, 3, 2, 4}, then the answer would be 2. The first increasing sequence would be {1, 3, 4} and the second would be {2}.
 */
 
-
-int maxAlternateSum(int arr[], int n) 
-{ 
-    if (n == 1) 
-        return arr[0]; 
-
-    int min = arr[0]; 
-    for (int i = 1; i < n; i++) { 
-        if (min > arr[i]) 
-            min = arr[i]; 
-    } 
-    if (min == arr[0]) { 
-        return arr[0]; 
-    } 
-
-    int dec[n]; 
-    memset(dec, 0, sizeof(dec)); 
-
-    int inc[n]; 
-    memset(inc, 0, sizeof(inc)); 
-
-    dec[0] = inc[0] = arr[0]; 
-  
-    int flag = 0; 
-    for (int i = 1; i < n; i++) { 
-        for (int j = 0; j < i; j++) { 
-            if (arr[j] > arr[i]) { 
-                dec[i] = max(dec[i], inc[j] + arr[i]); 
-                flag = 1; 
-            } 
-            else if (arr[j] < arr[i] && flag == 1) 
-                inc[i] = max(inc[i], dec[j] + arr[i]); 
-        } 
-    } 
-    int result = INT_MIN; 
-    for (int i = 0; i < n; i++) { 
-        if (result < inc[i]) 
-            result = inc[i]; 
-        if (result < dec[i]) 
-            result = dec[i]; 
-    } 
-    return result; 
-} 
+int MinimumNumIncreasingSubsequences(int arr[], int n)
+{
+    multiset<int> last;
+    for (int i = 0; i < n; i++) {
+        multiset<int>::iterator it = last.lower_bound(arr[i]);
+        if (it == last.begin())
+            last.insert(arr[i]);
+        else {
+            it--;
+            last.erase(it);
+            last.insert(arr[i]);
+        }
+    }
+    return last.size();
+}
 
 int main() 
 { 
-    int arr[] = { 8, 2, 3, 5, 7, 9, 10 }; 
+    int arr[] = {1, 3, 2, 4}; 
     int n = sizeof(arr) / sizeof(arr[0]); 
-    cout << "Sum of maximum sum alternating subsequence is "
-         << maxAlternateSum(arr, n) << endl; 
+    cout << MinimumNumIncreasingSubsequences(arr, n) << endl; 
     return 0; 
 }
