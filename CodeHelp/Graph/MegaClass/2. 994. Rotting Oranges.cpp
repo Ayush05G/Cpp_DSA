2742ea53119139e400ff35c9b8069d789e2b2f25
Ayush Gupta <ayush2425.rk@gmail.com>
2025-01-19 15:50:17 +0530

    Add functions to find the number of provinces, calculate the minimum minutes for rotting oranges, find the cheapest flight within K stops, and determine the longest cycle in a graph

---
+++ CodeHelp/Graph/MegaClass/2. 994. Rotting Oranges.cpp
@@ -0,0 +1,65 @@
+class Solution
+{
+public:
+    int orangesRotting(vector<vector<int>> &grid)
+    {
+        int rows = grid.size(), cols = grid[0].size();
+        queue<pair<int, int>> q;
+        int freshOranges = 0;
+
+        // Step 1: all the rotten oranges to the queue
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (grid[i][j] == 2)
+                    q.push({i, j});
+                else if (grid[i][j] == 1)
+                    freshOranges++;
+            }
+        }
+
+        if (!freshOranges)
+            return 0;
+
+        int minutes = 0;
+        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        // Step 2: Perform BFS
+        while (!q.empty())
+        {
+            int size = q.size();
+            bool hasRottenNew = false;
+
+            // processing current level / T / minute
+            for (int i = 0; i < size; i++)
+            {
+                auto curr = q.front();
+                q.pop();
+
+                for (auto dir : directions)
+                {
+                    int newRow = curr.first + dir.first;
+                    int newCol = curr.second + dir.second;
+
+                    // if adj cell is a fresh Orange, rot it.
+                    if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && grid[newRow][newCol] == 1)
+                    {
+                        grid[newRow][newCol] = 2;
+                        freshOranges--;
+                        q.push({newRow, newCol}); // for next level
+                        hasRottenNew = true;
+                    }
+                }
+            }
+
+            if (hasRottenNew)
+                minutes++;
+        }
+
+        // Step 3: checking
+        if (freshOranges > 0)
+            return -1;
+        return minutes;
+    }
+};
