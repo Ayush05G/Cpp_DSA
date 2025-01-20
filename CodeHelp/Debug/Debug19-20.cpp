ab683fe673eb5be5b3085786e4c683bc216edc2e
Ayush Gupta <ayush2425.rk@gmail.com>
2025-01-20 20:45:22 +0530

    Q4 (Refactor graph solution: replace BFS component retrieval with max area calculation in a grid)

--- CodeHelp/Debug/Debug19-20.cpp
+++ CodeHelp/Debug/Debug19-20.cpp
@@ -1,65 +1,44 @@
-#include <iostream>
-#include<bits/stdc++.h>
-using namespace std;
-
-vector<int> getOneComponent(bool** graph, int startingVertex, int v, bool* visited) {
-	vector<int> component;
-	queue<int> pendingNodes;
-	pendingNodes.push(startingVertex);
-	visited[startingVertex] = true;
-
-	while(!pendingNodes.empty()){
-		int current = pendingNodes.front();
-		pendingNodes.pop();
-		component.push_back(current);
-		for(int i=0; i<v; i++){
-			if(graph[current][i] && !visited[i]) {
-				pendingNodes.push(i);
-				visited[i] = true;
-			}
-		}
-
-	}
-	return component;
-}
-
-vector<vector<int>> getAllComponents(bool** graph, int v) {
-	vector<vector<int>> result;
-	bool* visited = new bool[v]();
-
-	for(int i=0; i<v; i++){
-		if(!visited[i]){
-			vector<int> component = getOneComponent(graph, i, v, visited);
-			result.push_back(component);
-		}
-	}
-
-	return result;
-}
-
-
-int main() {
-    int v, e;
-    cin >> v >> e;
-    bool** graph = new bool*[v];
-    for(int i=0; i<v; i++){
-        graph[i] = new bool[v]();
+class Solution {
+public:
+    vector<int>dx = {-1,1,0,0,1,1,-1,-1};
+    vector<int>dy = {0,0,-1,1,1,-1,1,-1};
+    
+    bool isValid(int x, int y, int n, int m){
+        return (x >= 0 and x < n and y >= 0 and y < m);
     }
-
-    for(int i=0; i<e; i++){
-        int a, b;
-        cin >> a >> b;
-        graph[a][b] = true;
-        graph[b][a] = true;
-    }
-
-    vector<vector<int>> components = getAllComponents(graph, v);
-    for(int i=0; i<components.size(); i++){
-        for(int j=0; j<components[i].size(); j++){
-            cout << components[i][j] << " ";
+    
+    int findMaxArea(vector<vector<int>>& grid) {
+        int ans = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int, int>>q;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1) {
+                    grid[i][j] = 0;
+                    int cnt = 1;  // Start with 1 for the current cell
+                    q.push({i,j});
+                    
+                    while(!q.empty()) {
+                        int x = q.front().first;
+                        int y = q.front().second;
+                        q.pop();  // Remove the processed element
+                        
+                        for(int k = 0; k < 8; k++) {
+                            int n_x = x + dx[k];
+                            int n_y = y + dy[k];
+                            if(isValid(n_x, n_y, n, m) and grid[n_x][n_y] == 1) {
+                                grid[n_x][n_y] = 0;
+                                cnt++;
+                                q.push({n_x, n_y});
+                            }
+                        }
+                    }
+                    ans = max(ans, cnt);
+                }
+            }
         }
-        cout << endl;
+        return ans;
     }
-
-    return 0;
-}
+};
