bff067821372c4d0ecc522a84305896bc4293d3d
Ayush Gupta <ayush2425.rk@gmail.com>
2025-01-20 20:39:55 +0530

    Q3 (Refactor graph traversal: add functions to retrieve connected components using BFS)

--- CodeHelp/Debug/Debug19-20.cpp
+++ CodeHelp/Debug/Debug19-20.cpp
@@ -1,33 +1,65 @@
-class Solution {
-public:
-    bool dfs(int source , int destination , vector<int> adj[] , vector<int> &vis)
-    {
-        vis[source]=1;
-        if(source==destination)
-        {
-            return true;
-        }
-        for(auto a:adj[source])
-        {
-            if(!vis[a])
-            {
-                if(dfs(a,destination , adj , vis))
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> getOneComponent(bool** graph, int startingVertex, int v, bool* visited) {
+	vector<int> component;
+	queue<int> pendingNodes;
+	pendingNodes.push(startingVertex);
+	visited[startingVertex] = true;
+
+	while(!pendingNodes.empty()){
+		int current = pendingNodes.front();
+		pendingNodes.pop();
+		component.push_back(current);
+		for(int i=0; i<v; i++){
+			if(graph[current][i] && !visited[i]) {
+				pendingNodes.push(i);
+				visited[i] = true;
+			}
+		}
+
+	}
+	return component;
+}
+
+vector<vector<int>> getAllComponents(bool** graph, int v) {
+	vector<vector<int>> result;
+	bool* visited = new bool[v]();
+
+	for(int i=0; i<v; i++){
+		if(!visited[i]){
+			vector<int> component = getOneComponent(graph, i, v, visited);
+			result.push_back(component);
+		}
+	}
+
+	return result;
+}
+
+
+int main() {
+    int v, e;
+    cin >> v >> e;
+    bool** graph = new bool*[v];
+    for(int i=0; i<v; i++){
+        graph[i] = new bool[v]();
     }
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) 
-    {
-        vector<int> vis(n,0);
-        vector<int> adj[n];
-        for(int i=0;i<edges.size();i++)
-        {
-            adj[edges[i][1]].push_back(edges[i][0]); 
-            adj[edges[i][0]].push_back(edges[i][1]);            
-        }
-        return dfs(source , destination , adj , vis);
+
+    for(int i=0; i<e; i++){
+        int a, b;
+        cin >> a >> b;
+        graph[a][b] = true;
+        graph[b][a] = true;
     }
-};
+
+    vector<vector<int>> components = getAllComponents(graph, v);
+    for(int i=0; i<components.size(); i++){
+        for(int j=0; j<components[i].size(); j++){
+            cout << components[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
