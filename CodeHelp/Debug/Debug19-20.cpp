ee25bdfd845641e5189c262457e20c4fb3911242
Ayush Gupta <ayush2425.rk@gmail.com>
2025-01-20 21:01:59 +0530

    Q7 (Refactor graph implementation: streamline edge handling and integrate Kruskal's algorithm for minimum spanning tree)

--- CodeHelp/Debug/Debug19-20.cpp
+++ CodeHelp/Debug/Debug19-20.cpp
@@ -2,124 +2,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Data structure to store a graph edge
-struct Edge {
-    int src, dest;
+class Graph {
+  private:
+  vector<pair<int, edge> > G;
+  vector<pair<int, edge> > T;  // mst
+  int *parent;
+  int V;
+
+  public:
+  Graph(int V);
+  void AddWeightedEdge(int u, int v, int w);
+  int find_set(int i);
+  void union_set(int u, int v);
+  void kruskal();
 };
+Graph::Graph(int V) {
+  parent = new int[V];
+  for (int i = 0; i < V; i++)
+    parent[i] = i;
 
-// Class to represent a graph object
-class Graph
-{ 
-public:
-    // a vector of vectors to represent an adjacency list
-    vector<vector<int>> adjList;
- 
-    // Graph Constructor
-    Graph(vector<Edge> const &edges, int N)
-    {
-        // resize the vector to N elements of type vector<int>
-        adjList.resize(N);
- 
-        // add edges to the directed graph
-        for (auto &edge: edges)
-        {
-            // insert at the end
-            adjList[edge.src].push_back(edge.dest);
-        }
-    }
-};
-
-// Data structure to store a Disjoint Set
-class DisjointSet
-{
-    unordered_map<int, int> parent;
-
-public:
-    // perform MakeSet operation
-    void MakeSet(int N)
-    {
-        // create N disjoint sets (one for each vertex)
-        for (int i = 0; i < N; i++) {
-            parent[i] = i;
-        }
-    }
- 
-    // Find the root of the set in which element k belongs
-    int Find(int k)
-    {
-        // if k is root
-        if (parent[k] == k) {
-            return k;
-        }
- 
-        // recur for parent until we find the root
-        return Find(parent[k]);
-    }
- 
-    // Perform Union of two subsets
-    void Union(int a, int b)
-    {
-        // find the root of the sets in which elements
-        // x and y belongs
-        int x = Find(a);
-        int y = Find(b);
- 
-        parent[x] = y;
-    }
-};
-
-bool findCycle(Graph const &graph, int n)
-{
-    // initialize Main class
-    DisjointSet ds;
- 
-    // create a singleton set for each element of the universe
-    ds.MakeSet(n);
- 
-    // consider every edge (u, v)
-    for (int u = 0; u < n; u++)
-    {
-        // Recur for all adjacent vertices
-        for (int v: graph.adjList[u])
-        {
-            // find the root of the sets to which elements `u` and `v` belongs
-            int x = ds.Find(u);
-            int y = ds.Find(v);
- 
-            // if both `u` and `v` have the same parent, the cycle is found
-            if (x == y) {
-                return true;
-            }
-            else {
-                ds.Union(x, y);
-            }
-        }
-    }
- 
-    return false;
+  G.clear();
+  T.clear();
+}
+void Graph::AddWeightedEdge(int u, int v, int w) {
+  G.push_back(make_pair(w, edge(u, v)));
+}
+int Graph::find_set(int i) {
+  if (i == parent[i])
+    return i;
+  else
+    return find_set(parent[i]);
 }
 
-int main()
-{
-    // initialize edges as per the above diagram
-    vector<Edge> edges =
-    {
-        {0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}
-    };
- 
-    // total number of nodes in the graph
-    int N = 5;
- 
-    // build a graph from the given edges
-    Graph graph(edges, N);
- 
-    // check if the graph contains a cycle
-    if (findCycle(graph, N)) {
-        cout << "The graph contains a cycle";
+void Graph::union_set(int u, int v) {
+  parent[u] = parent[v];
+}
+void Graph::kruskal() {
+  int i, uRep, vRep;
+  sort(G.begin(), G.end());  // increasing weight
+  for (i = 0; i < G.size(); i++) {
+    uRep = find_set(G[i].second.first);
+    vRep = find_set(G[i].second.second);
+    if (uRep != vRep) {
+      T.push_back(G[i]);  // add to tree
+      union_set(uRep, vRep);
     }
-    else {
-        cout << "The graph doesn't contain any cycle";
-    }
- 
-    return 0;
+  }
 }
